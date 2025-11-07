a346b95958c7f1efa88e9029b29fe89fc05af4f3
zayn <siripurapumahesh7670@gmail.com>
2025-11-07 15:38:24 +0530

    this is my first commit

--- src/main/java/com/java/App.java
+++ src/main/java/com/java/App.java
@@ -3,5 +3,7 @@ package com.java;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    
+    System.out.println("welcome to java application-1");
   }
 }
